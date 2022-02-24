809e04644bb47aac797b3fe4ed67e8a40b7d7223
Sangbin Lee <leesangbin@outlook.com>
2022-02-24 22:31:31 +0100

    add console app demo

---
+++ MapleUtils.Parser.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MapleUtils.Parser.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                using var mapleutilsParser = MapleUtilsParser.Create(isHeadless: false);
+                var test = mapleutilsParser.GetCharacterAsync("상빈").Result;
+                Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(test));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
